264dc37b3ad397230d93aeb1006e01ec97cb3b11
trash <toidi357@gmail.com>
2024-06-30 01:38:50 -0700

    works!

--- src/background.js
+++ src/background.js
@@ -10,18 +10,18 @@ function setIconBasedOnTab(tabId, tabUrl) {
         chrome.action.setIcon({
             tabId: tabId,
             path: {
-                "16": "images/normal.png",
-                "48": "images/normal.png",
-                "128": "images/normal.png"
+                "16": "/images/normal.png",
+                "48": "/images/normal.png",
+                "128": "/images/normal.png"
             }
         });
     } else {
         chrome.action.setIcon({
             tabId: tabId,
             path: {
-                "16": "images/grey.png",
-                "48": "images/grey.png",
-                "128": "images/grey.png"
+                "16": "/images/grey.png",
+                "48": "/images/grey.png",
+                "128": "/images/grey.png"
             }
         });
     }
@@ -69,9 +69,6 @@ chrome.runtime.onMessage.addListener(function (message, sender) {
 chrome.action.onClicked.addListener(async tab => {
     // only initiates the download if you're on bruinlearn and a sub worker has found a valid video
     if ((new URL(tab.url)).hostname == 'bruinlearn.ucla.edu' && VIDEOFOUND) {
-        // used for error handling in the catch block
-        const controller = new AbortController();
-        const signal = controller.signal;
         try {
             const BASEURL = VIDEOURL.substring(0, VIDEOURL.indexOf('playlist.m3u8'))
             let response = await fetch(VIDEOURL)
@@ -91,34 +88,20 @@ chrome.action.onClicked.addListener(async tab => {
             var links = []
             var i = 0; // this i is used to prevent race conditions given async downloading
             data.split('\n').forEach(line => {
-                if (line.startsWith('media')) links.push([i, line])
+                if (line.startsWith('media')) {
+                    links.push([i, BASEURL + line])
+                    i++
+                }
             })
 
-            // begin downloading
-            chrome.action.setBadgeText({ text: "loading" });
-            chrome.action.setBadgeBackgroundColor({ color: [0, 255, 255, 255] })
+            // must send the downloading the main.js frame because URL.createObjectURL only valid in content scripts
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                // since only one tab should be active and in the current window at once
+                // the return variable should only have one entry
+                var activeTab = tabs[0];
+                chrome.tabs.sendMessage(activeTab.id, { isDownload: true, data: links })
+            });
 
-            var blobs = new Array(links.length)
-            // must send the downloading and ffmpeg-ing to the main.js frame because ffmpeg requires a content script
-            const downloadLinks = async (links) => {
-                const promises = links.map(async linkObj => {
-                    // fetch
-                    var response = await fetch(BASEURL + linkObj[1], { signal: signal });
-                    var blob = await response.blob();
-                    // slap this blob into the global array
-                    blobs[linkObj[0]] = blob;
-                })
-                await Promise.all(promises);
-            }
-            await downloadLinks(links);
-
-            // combine all the blobs into one
-            var final = new Blob(blobs, { type: "video/mp2t" });
-            // download
-            var downloadLink = URL.createObjectURL(final) 
-            chrome.downloads.download({ url: downloadLink });
-
-            chrome.action.setBadgeText({ text: '' });            
         } catch (err) {
             console.error(err);
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -127,9 +110,6 @@ chrome.action.onClicked.addListener(async tab => {
                 var activeTab = tabs[0];
                 chrome.tabs.sendMessage(activeTab.id, { alertError: true, data: 'Error downloading video' })
             });
-
-            // clear all pending network requests
-            controller.abort();
         }
 
     }
@@ -143,65 +123,3 @@ chrome.action.onClicked.addListener(async tab => {
 
     }
 });
-
-
-
-/*
-const { FFmpeg } = FFmpegWASM;
-const { fetchFile } = FFmpegUtil;
-
-// initialize ffmpeg
-const ffmpeg = new FFmpeg();
-
-// convert wasm and core url to absolute path
-const coreUrl = chrome.runtime.getURL("lib/ffmpeg/ffmpeg-core.js");
-const wasmUrl = chrome.runtime.getURL("lib/ffmpeg/ffmpeg-core.wasm");
-
-// log ffmpeg messages
-ffmpeg.on("log", ({ message }) => {
-    console.log(message);
-});
-
-// progress bar
-ffmpeg.on("progress", ({ progress, time }) => {
-    console.log((progress * 100) + "%, time: " + (time / 1000000) + " s");
-});
-
-// custom ffmpeg command
-async function runFFmpeg(inputFileName, outputFileName, commandStr, file) {
-    console.log(inputFileName, outputFileName, commandStr, file);
-
-    // exit ffmpeg if it is already loaded
-    if (ffmpeg.loaded) {
-        await ffmpeg.terminate();
-    }
-
-    // load ffmpeg
-    await ffmpeg.load({
-        coreURL: coreUrl,
-        wasmURL: wasmUrl,
-    });
-
-    // split command string
-    const commandList = commandStr.split(' ');
-    if (commandList.shift() !== 'ffmpeg') {
-        alert('Please start with ffmpeg');
-        return;
-    }
-
-    // write file to filesystem
-    await ffmpeg.writeFile(inputFileName, await fetchFile(file));
-
-    // execute command
-    console.log(commandList);
-    await ffmpeg.exec(commandList);
-
-    // read output file
-    const data = await ffmpeg.readFile(outputFileName);
-
-    // create blob and download
-    const blob = new Blob([data.buffer]);
-    console.log(blob);
-    downloadFile(blob, outputFileName);
-}
-*/
--- src/main.js
+++ src/main.js
@@ -7,59 +7,41 @@ chrome.runtime.onMessage.addListener(function (message, sender) {
     }
 });
 
-/*
- * used to download videos and load ffmpeg
-*/
-// Initialize FFmpeg WASM
-async function initFFmpeg() {
-    debugger;
-    const { createFFmpeg, fetchFile } = FFmpeg;
-    const ffmpeg = createFFmpeg({
-        log: true,
-        corePath: chrome.runtime.getURL('lib/ffmpeg-core.js'),
-    });
-
-    if (ffmpeg.isLoaded()) await ffmpeg.terminate();
-
-    await ffmpeg.load();
-
-    return ffmpeg;
-}
-
 // listen for download alerts
 chrome.runtime.onMessage.addListener(async (message, sender) => {
     if (message.isDownload) {
-        // used for error handling in the catch() block
+        // used for error handling in the catch block
         const controller = new AbortController();
         const signal = controller.signal;
-
         try {
-            // Initialize FFmpeg WASM
-            const ffmpeg = await initFFmpeg();
-
+            const blobs = new Array(message.data.length)
             const downloadLinks = async (links) => {
                 const promises = links.map(async linkObj => {
                     // fetch
-                    var response = await fetch(BASEURL + linkObj[1], { signal: signal });
+                    var response = await fetch(linkObj[1], { signal: signal });
                     var blob = await response.blob();
-                    var arrayBuffer = await new Response(blob).arrayBuffer();
-                    // write to FFMPEG FS
-                    ffmpeg.FS('writeFile', linkObj[0] + ".ts", new Uint8Array(arrayBuffer));
+                    // slap this blob into the global array
+                    blobs[linkObj[0]] = blob;
                 })
                 await Promise.all(promises);
             }
-
             await downloadLinks(message.data);
-            console.log('huh')
 
-            const files = await ffmpeg.FS('readdir', '/');
-            console.log(files);
+            // combine all the blobs into one
+            var final = new Blob(blobs, { type: "video/mp2t" });
+
+            // do the downloading
+            var downloadLink = URL.createObjectURL(final)
+            var a = document.createElement('a');
+            a.href = downloadLink;
+            a.download = 'video.ts'; // set file name
+            document.body.appendChild(a);
+            a.click();
+            delete a;
         }
-        catch (err) {
-            // logging purposes
-            console.error(err);
-            alert('Error downloading video');
-
+        catch (e) {
+            console.error(e);
+            alert('Error downloading video')
             // clear all pending network requests
             controller.abort();
         }
